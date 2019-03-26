2863a57b329f49345596a5bf2763573da56251ee
Павел Хрусталев <Pavel@MacBook-Air-Pavel.local>
2019-03-26 13:37:08 +0300

    Updates

---
+++ precache-manifest.2eacfa69113626b819ae0044de4261b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca14c73406c982b0870b",
+    "url": "/weather-app/static/css/main.97a7f340.chunk.css"
+  },
+  {
+    "revision": "ca14c73406c982b0870b",
+    "url": "/weather-app/static/js/main.ca14c734.chunk.js"
+  },
+  {
+    "revision": "70ff29b54e6aee2bd8e0",
+    "url": "/weather-app/static/js/1.70ff29b5.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "a1a0e8f83b4b82e502449b5f98036e6f",
+    "url": "/weather-app/index.html"
+  }
+];
